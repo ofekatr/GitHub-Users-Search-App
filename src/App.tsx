8e0ecba614c85c89b696cfd48e7eb84c63de6310
Ofek Atar <ofekatar98@gmail.com>
2021-01-02 14:29:28 +0200

    Defined project structure, added Main component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
-    
+    <>
+      <Main />
+    </>
   );
 }
 
