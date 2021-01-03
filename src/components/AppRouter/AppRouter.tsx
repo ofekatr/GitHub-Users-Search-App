63d930ca3da7abe816f8ac3c47bcf8f8b9284a77
Ofek Atar <ofekatar98@gmail.com>
2021-01-03 10:48:38 +0200

    Added home page.

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -6,13 +6,15 @@ import {
   Switch,
 } from "react-router-dom";
 
+import HomePage from "../../pages/HomePage/HomePage";
 import SearchPage from "../../pages/SearchPage/SearchPage";
 
 export default function AppRouter() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={SearchPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/search" component={SearchPage} />
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./HomePage.scss";
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to usertron!</h1>
+      <img src={process.env.PUBLIC_URL + "/img/github-logo.png"} alt="github-logo" />
+      <div className="search-page-link">
+        <Link to="/search">
+          <button>Start Searching</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
